3ad4cc2831a3dfb7f4ef4e89f71831477b90b25b
wgh@qq.com <964265236@qq.com>
2019-08-02 09:13:24 +0800

    基本结构

---
+++ src/pages/login/index.js
@@ -0,0 +1,13 @@
+ import React, { Component } from 'react'
+ import './login.less'
+
+export default class index extends Component {
+    render() {
+        return (
+            <div className="login">
+                <header className="login-header"></header>
+                <section className="login-section"></section>
+            </div>
+        )
+    }
+}
